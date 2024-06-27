718fa7d497397ac829ff66ea35fd93349017e0a3
quypn23 <141406805+quypn23@users.noreply.github.com>
2024-06-27 17:33:03 +0700

    Update librediscluster.sh

--- containers/bitnami/redis-cluster/7.2/debian-12/rootfs/opt/bitnami/scripts/librediscluster.sh
+++ containers/bitnami/redis-cluster/7.2/debian-12/rootfs/opt/bitnami/scripts/librediscluster.sh
@@ -2,33 +2,33 @@
 # Copyright Broadcom, Inc. All Rights Reserved.
 # SPDX-License-Identifier: APACHE-2.0
 #
-# REDIS Redis Cluster library
+# redis Redis Cluster library
 
 # shellcheck disable=SC1090,SC1091
 
 # Load Generic Libraries
-. /opt/REDIS/scripts/libfile.sh
-. /opt/REDIS/scripts/libfs.sh
-. /opt/REDIS/scripts/liblog.sh
-. /opt/REDIS/scripts/libnet.sh
-. /opt/REDIS/scripts/libos.sh
-. /opt/REDIS/scripts/libservice.sh
-. /opt/REDIS/scripts/libvalidations.sh
-. /opt/REDIS/scripts/libredis.sh
+. /opt/redis/scripts/libfile.sh
+. /opt/redis/scripts/libfs.sh
+. /opt/redis/scripts/liblog.sh
+. /opt/redis/scripts/libnet.sh
+. /opt/redis/scripts/libos.sh
+. /opt/redis/scripts/libservice.sh
+. /opt/redis/scripts/libvalidations.sh
+. /opt/redis/scripts/libredis.sh
 
 # Functions
 
 ########################
-# Validate settings in REDIS_* env vars.
+# Validate settings in redis_* env vars.
 # Globals:
-#   REDIS_*
+#   redis_*
 # Arguments:
 #   None
 # Returns:
 #   None
 #########################
 redis_cluster_validate() {
-    debug "Validating settings in REDIS_* env vars.."
+    debug "Validating settings in redis_* env vars.."
     local error_code=0
 
     # Auxiliary functions
@@ -47,25 +47,25 @@ redis_cluster_validate() {
     if is_boolean_yes "$ALLOW_EMPTY_PASSWORD"; then
         empty_password_enabled_warn
     else
-        [[ -z "$REDIS_PASSWORD" ]] && empty_password_error REDIS_PASSWORD
+        [[ -z "$redis_PASSWORD" ]] && empty_password_error redis_PASSWORD
     fi
 
-    if ! is_boolean_yes "$REDIS_CLUSTER_DYNAMIC_IPS"; then
-        [[ -z "$REDIS_CLUSTER_ANNOUNCE_IP" ]] && print_validation_error "To provide external access you need to provide the REDIS_CLUSTER_ANNOUNCE_IP env var"
+    if ! is_boolean_yes "$redis_CLUSTER_DYNAMIC_IPS"; then
+        [[ -z "$redis_CLUSTER_ANNOUNCE_IP" ]] && print_validation_error "To provide external access you need to provide the redis_CLUSTER_ANNOUNCE_IP env var"
     fi
 
-    [[ -z "$REDIS_NODES" ]] && print_validation_error "REDIS_NODES is required"
+    [[ -z "$redis_NODES" ]] && print_validation_error "redis_NODES is required"
 
-    if [[ -z "$REDIS_PORT_NUMBER" ]]; then
-        print_validation_error "REDIS_PORT_NUMBER cannot be empty"
+    if [[ -z "$redis_PORT_NUMBER" ]]; then
+        print_validation_error "redis_PORT_NUMBER cannot be empty"
     fi
 
-    if is_boolean_yes "$REDIS_CLUSTER_CREATOR"; then
-        [[ -z "$REDIS_CLUSTER_REPLICAS" ]] && print_validation_error "To create the cluster you need to provide the number of replicas to the cluster creator"
+    if is_boolean_yes "$redis_CLUSTER_CREATOR"; then
+        [[ -z "$redis_CLUSTER_REPLICAS" ]] && print_validation_error "To create the cluster you need to provide the number of replicas to the cluster creator"
     fi
 
-    if ((REDIS_CLUSTER_SLEEP_BEFORE_DNS_LOOKUP < 0)); then
-        print_validation_error "REDIS_CLUSTER_SLEEP_BEFORE_DNS_LOOKUP must be greater or equal to zero"
+    if ((redis_CLUSTER_SLEEP_BEFORE_DNS_LOOKUP < 0)); then
+        print_validation_error "redis_CLUSTER_SLEEP_BEFORE_DNS_LOOKUP must be greater or equal to zero"
     fi
 
     [[ "$error_code" -eq 0 ]] || exit "$error_code"
@@ -74,7 +74,7 @@ redis_cluster_validate() {
 ########################
 # Redis specific configuration to override the default one
 # Globals:
-#   REDIS_*
+#   redis_*
 # Arguments:
 #   None
 # Returns:
@@ -82,42 +82,42 @@ redis_cluster_validate() {
 #########################
 redis_cluster_override_conf() {
     # Redis configuration to override
-    if ! is_boolean_yes "$REDIS_CLUSTER_DYNAMIC_IPS"; then
-        redis_conf_set cluster-announce-ip "$REDIS_CLUSTER_ANNOUNCE_IP"
+    if ! is_boolean_yes "$redis_CLUSTER_DYNAMIC_IPS"; then
+        redis_conf_set cluster-announce-ip "$redis_CLUSTER_ANNOUNCE_IP"
     fi
-    if is_boolean_yes "$REDIS_CLUSTER_DYNAMIC_IPS"; then
+    if is_boolean_yes "$redis_CLUSTER_DYNAMIC_IPS"; then
         # Always set the announce-ip to avoid issues when using proxies and traffic restrictions.
         redis_conf_set cluster-announce-ip "$(get_machine_ip)"
     fi
-    if ! is_empty_value "$REDIS_CLUSTER_ANNOUNCE_HOSTNAME"; then
-        redis_conf_set "cluster-announce-hostname" "$REDIS_CLUSTER_ANNOUNCE_HOSTNAME"
+    if ! is_empty_value "$redis_CLUSTER_ANNOUNCE_HOSTNAME"; then
+        redis_conf_set "cluster-announce-hostname" "$redis_CLUSTER_ANNOUNCE_HOSTNAME"
     fi
-    if ! is_empty_value "$REDIS_CLUSTER_PREFERRED_ENDPOINT_TYPE"; then
-        redis_conf_set "cluster-preferred-endpoint-type" "$REDIS_CLUSTER_PREFERRED_ENDPOINT_TYPE"
+    if ! is_empty_value "$redis_CLUSTER_PREFERRED_ENDPOINT_TYPE"; then
+        redis_conf_set "cluster-preferred-endpoint-type" "$redis_CLUSTER_PREFERRED_ENDPOINT_TYPE"
     fi
-    if is_boolean_yes "$REDIS_TLS_ENABLED"; then
+    if is_boolean_yes "$redis_TLS_ENABLED"; then
         redis_conf_set tls-cluster yes
         redis_conf_set tls-replication yes
     fi
-    if ! is_empty_value "$REDIS_CLUSTER_ANNOUNCE_PORT"; then
-        redis_conf_set "cluster-announce-port" "$REDIS_CLUSTER_ANNOUNCE_PORT"
+    if ! is_empty_value "$redis_CLUSTER_ANNOUNCE_PORT"; then
+        redis_conf_set "cluster-announce-port" "$redis_CLUSTER_ANNOUNCE_PORT"
     fi
-    if ! is_empty_value "$REDIS_CLUSTER_ANNOUNCE_BUS_PORT"; then
-        redis_conf_set "cluster-announce-bus-port" "$REDIS_CLUSTER_ANNOUNCE_BUS_PORT"
+    if ! is_empty_value "$redis_CLUSTER_ANNOUNCE_BUS_PORT"; then
+        redis_conf_set "cluster-announce-bus-port" "$redis_CLUSTER_ANNOUNCE_BUS_PORT"
     fi
     # Multithreading configuration
-    if ! is_empty_value "$REDIS_IO_THREADS_DO_READS"; then
-        redis_conf_set "io-threads-do-reads" "$REDIS_IO_THREADS_DO_READS"
+    if ! is_empty_value "$redis_IO_THREADS_DO_READS"; then
+        redis_conf_set "io-threads-do-reads" "$redis_IO_THREADS_DO_READS"
     fi
-    if ! is_empty_value "$REDIS_IO_THREADS"; then
-        redis_conf_set "io-threads" "$REDIS_IO_THREADS"
+    if ! is_empty_value "$redis_IO_THREADS"; then
+        redis_conf_set "io-threads" "$redis_IO_THREADS"
     fi
 }
 
 ########################
 # Ensure Redis is initialized
 # Globals:
-#   REDIS_*
+#   redis_*
 # Arguments:
 #   None
 # Returns:
@@ -131,7 +131,7 @@ redis_cluster_initialize() {
 ########################
 # Creates the Redis cluster
 # Globals:
-#   REDIS_*
+#   redis_*
 # Arguments:
 #   - $@ Array with the hostnames
 # Returns:
@@ -146,8 +146,8 @@ redis_cluster_create() {
     for node in "${nodes[@]}"; do
         read -r -a host_and_port <<< "$(to_host_and_port "$node")"
         wait_command="timeout 5 redis-cli -h ${host_and_port[0]} -p ${host_and_port[1]} ping"
-        if is_boolean_yes "$REDIS_TLS_ENABLED"; then
-            wait_command="${wait_command:0:-5} --tls --cert ${REDIS_TLS_CERT_FILE} --key ${REDIS_TLS_KEY_FILE} --cacert ${REDIS_TLS_CA_FILE} ping"
+        if is_boolean_yes "$redis_TLS_ENABLED"; then
+            wait_command="${wait_command:0:-5} --tls --cert ${redis_TLS_CERT_FILE} --key ${redis_TLS_KEY_FILE} --cacert ${redis_TLS_CA_FILE} ping"
         fi
         while [[ $($wait_command) != 'PONG' ]]; do
             echo "Node $node not ready, waiting for all the nodes to be ready..."
@@ -155,17 +155,17 @@ redis_cluster_create() {
         done
     done
 
-    echo "Waiting ${REDIS_CLUSTER_SLEEP_BEFORE_DNS_LOOKUP}s before querying node ip addresses"
-    sleep "${REDIS_CLUSTER_SLEEP_BEFORE_DNS_LOOKUP}"
+    echo "Waiting ${redis_CLUSTER_SLEEP_BEFORE_DNS_LOOKUP}s before querying node ip addresses"
+    sleep "${redis_CLUSTER_SLEEP_BEFORE_DNS_LOOKUP}"
 
     for node in "${nodes[@]}"; do
         read -r -a host_and_port <<< "$(to_host_and_port "$node")"
-        sockets+=("$(wait_for_dns_lookup "${host_and_port[0]}" "${REDIS_CLUSTER_DNS_LOOKUP_RETRIES}" "${REDIS_CLUSTER_DNS_LOOKUP_SLEEP}"):${host_and_port[1]}")
+        sockets+=("$(wait_for_dns_lookup "${host_and_port[0]}" "${redis_CLUSTER_DNS_LOOKUP_RETRIES}" "${redis_CLUSTER_DNS_LOOKUP_SLEEP}"):${host_and_port[1]}")
     done
 
-    create_command="redis-cli --cluster create ${sockets[*]} --cluster-replicas ${REDIS_CLUSTER_REPLICAS} --cluster-yes"
-    if is_boolean_yes "$REDIS_TLS_ENABLED"; then
-        create_command="${create_command} --tls --cert ${REDIS_TLS_CERT_FILE} --key ${REDIS_TLS_KEY_FILE} --cacert ${REDIS_TLS_CA_FILE}"
+    create_command="redis-cli --cluster create ${sockets[*]} --cluster-replicas ${redis_CLUSTER_REPLICAS} --cluster-yes"
+    if is_boolean_yes "$redis_TLS_ENABLED"; then
+        create_command="${create_command} --tls --cert ${redis_TLS_CERT_FILE} --key ${redis_TLS_KEY_FILE} --cacert ${redis_TLS_CA_FILE}"
     fi
     yes yes | $create_command || true
     if redis_cluster_check "${sockets[0]}"; then
@@ -181,8 +181,8 @@ redis_cluster_create() {
 ##  - $1: node where to check the cluster state
 #########################
 redis_cluster_check() {
-    if is_boolean_yes "$REDIS_TLS_ENABLED"; then
-        local -r check=$(redis-cli --tls --cert "${REDIS_TLS_CERT_FILE}" --key "${REDIS_TLS_KEY_FILE}" --cacert "${REDIS_TLS_CA_FILE}" --cluster check "$1")
+    if is_boolean_yes "$redis_TLS_ENABLED"; then
+        local -r check=$(redis-cli --tls --cert "${redis_TLS_CERT_FILE}" --key "${redis_TLS_KEY_FILE}" --cacert "${redis_TLS_CA_FILE}" --cluster check "$1")
     else
         local -r check=$(redis-cli --cluster check "$1")
     fi
@@ -196,49 +196,49 @@ redis_cluster_check() {
 #########################
 ## Recovers the cluster when using dynamic IPs by changing them in the nodes.conf
 # Globals:
-#   REDIS_*
+#   redis_*
 # Arguments:
 #   None
 # Returns:
 #   None
 #########################
 redis_cluster_update_ips() {
-    read -ra nodes <<< "$(tr ',;' ' ' <<< "${REDIS_NODES}")"
+    read -ra nodes <<< "$(tr ',;' ' ' <<< "${redis_NODES}")"
     declare -A host_2_ip_array # Array to map hosts and IPs
     # Update the IPs when a number of nodes > quorum change their IPs
-    if [[ ! -f "${REDIS_DATA_DIR}/nodes.sh" || ! -f "${REDIS_DATA_DIR}/nodes.conf" ]]; then
+    if [[ ! -f "${redis_DATA_DIR}/nodes.sh" || ! -f "${redis_DATA_DIR}/nodes.conf" ]]; then
         # It is the first initialization so store the nodes
         for node in "${nodes[@]}"; do
             read -r -a host_and_port <<< "$(to_host_and_port "$node")"
-            ip=$(wait_for_dns_lookup "${host_and_port[0]}" "$REDIS_DNS_RETRIES" 5)
+            ip=$(wait_for_dns_lookup "${host_and_port[0]}" "$redis_DNS_RETRIES" 5)
             host_2_ip_array["$node"]="$ip"
         done
         echo "Storing map with hostnames and IPs"
-        declare -p host_2_ip_array >"${REDIS_DATA_DIR}/nodes.sh"
+        declare -p host_2_ip_array >"${redis_DATA_DIR}/nodes.sh"
     else
         # The cluster was already started
-        . "${REDIS_DATA_DIR}/nodes.sh"
+        . "${redis_DATA_DIR}/nodes.sh"
         # Update the IPs in the nodes.conf
         for node in "${nodes[@]}"; do
             read -r -a host_and_port <<< "$(to_host_and_port "$node")"
-            newIP=$(wait_for_dns_lookup "${host_and_port[0]}" "$REDIS_DNS_RETRIES" 5)
+            newIP=$(wait_for_dns_lookup "${host_and_port[0]}" "$redis_DNS_RETRIES" 5)
             # The node can be new if we are updating the cluster, so catch the unbound variable error
             if [[ ${host_2_ip_array[$node]+true} ]]; then
                 info "Changing old IP ${host_2_ip_array[$node]} by the new one ${newIP}"
-                nodesFile=$(sed "s/ ${host_2_ip_array[$node]}:/ $newIP<NEWIP>:/g" "${REDIS_DATA_DIR}/nodes.conf")
-                echo "$nodesFile" >"${REDIS_DATA_DIR}/nodes.conf"
+                nodesFile=$(sed "s/ ${host_2_ip_array[$node]}:/ $newIP<NEWIP>:/g" "${redis_DATA_DIR}/nodes.conf")
+                echo "$nodesFile" >"${redis_DATA_DIR}/nodes.conf"
             fi
             host_2_ip_array["$node"]="$newIP"
         done
-        replace_in_file "${REDIS_DATA_DIR}/nodes.conf" "<NEWIP>:" ":" false
-        declare -p host_2_ip_array >"${REDIS_DATA_DIR}/nodes.sh"
+        replace_in_file "${redis_DATA_DIR}/nodes.conf" "<NEWIP>:" ":" false
+        declare -p host_2_ip_array >"${redis_DATA_DIR}/nodes.sh"
     fi
 }
 
 #########################
 ## Assigns a port to the host if one is not set using redis defaults
 # Globals:
-#   REDIS_*
+#   redis_*
 # Arguments:
 #   $1 - redis host or redis host and port
 # Returns:
@@ -249,10 +249,10 @@ to_host_and_port() {
     read -r -a host_and_port <<< "$(echo "$host" | tr ":" " ")"
 
     if [ "${#host_and_port[*]}" -eq "1" ]; then
-        if is_boolean_yes "$REDIS_TLS_ENABLED"; then
-            host_and_port=("${host_and_port[0]}" "${REDIS_TLS_PORT_NUMBER}")
+        if is_boolean_yes "$redis_TLS_ENABLED"; then
+            host_and_port=("${host_and_port[0]}" "${redis_TLS_PORT_NUMBER}")
         else
-            host_and_port=("${host_and_port[0]}" "${REDIS_PORT_NUMBER}")
+            host_and_port=("${host_and_port[0]}" "${redis_PORT_NUMBER}")
         fi
     fi
 
