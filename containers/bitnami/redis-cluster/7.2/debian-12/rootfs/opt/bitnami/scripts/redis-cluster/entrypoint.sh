90a4e6e199f3d0fd9fb6eda93d61f60f94a1324e
quypn23 <141406805+quypn23@users.noreply.github.com>
2024-06-27 17:33:43 +0700

    Update entrypoint.sh

--- containers/bitnami/redis-cluster/7.2/debian-12/rootfs/opt/bitnami/scripts/redis-cluster/entrypoint.sh
+++ containers/bitnami/redis-cluster/7.2/debian-12/rootfs/opt/bitnami/scripts/redis-cluster/entrypoint.sh
@@ -10,23 +10,23 @@ set -o pipefail
 # set -o xtrace # Uncomment this line for debugging purposes
 
 # Load Redis environment variables
-. /opt/bitnami/scripts/redis-cluster-env.sh
+. /opt/redis/scripts/redis-cluster-env.sh
 
 # Load libraries
-. /opt/bitnami/scripts/libbitnami.sh
-. /opt/bitnami/scripts/librediscluster.sh
+. /opt/redis/scripts/libredis.sh
+. /opt/redis/scripts/librediscluster.sh
 
 print_welcome_page
 
 # We add the copy from default config in the entrypoint to not break users 
 # bypassing the setup.sh logic. If the file already exists do not overwrite (in
-# case someone mounts a configuration file in /opt/bitnami/redis/etc)
+# case someone mounts a configuration file in /opt/redis/redis/etc)
 debug "Copying files from $REDIS_DEFAULT_CONF_DIR to $REDIS_CONF_DIR"
 cp -nr "$REDIS_DEFAULT_CONF_DIR"/. "$REDIS_CONF_DIR"
 
 if [[ "$*" = *"/run.sh"* ]]; then
     info "** Starting Redis setup **"
-    /opt/bitnami/scripts/redis-cluster/setup.sh
+    /opt/redis/scripts/redis-cluster/setup.sh
     info "** Redis setup finished! **"
 fi
 
